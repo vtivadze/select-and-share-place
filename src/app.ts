0b9882f10d4fabfed29906563b016f00bc8fb7f5
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-11 12:20:44 +0400

    Create project skeleton

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector('form')! as HTMLFormElement;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  console.log(enteredAddress);
+  // send
+}
+
+form.addEventListener('submit', searchAddressHandler);
