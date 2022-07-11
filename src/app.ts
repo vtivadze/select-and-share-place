92978b5b8739087f375ac83c8a25ba707f5a8475
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-11 13:29:35 +0400

    Get Google API key

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
+import { GOOGLE_API_KEY } from '../config';
+
 const form = document.querySelector('form')! as HTMLFormElement;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
-  console.log(enteredAddress);
+  console.log(enteredAddress, GOOGLE_API_KEY);
   // send
 }
 
